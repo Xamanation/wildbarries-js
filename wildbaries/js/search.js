4e4459fd05e67916d92fe12eb8f92b205f4a0671
Komarin Artem <69342500+Xamanation@users.noreply.github.com>
2022-02-24 22:44:49 +0500

    Delete wildbaries directory

--- wildbaries/js/search.js
+++
@@ -1,9 +0,0 @@
-const search = function () {
-  const input = document.querySelector('.search-block > input')
-  const searchBtn = document.querySelector('.search-block > button')
-
-  searchBtn.addEventListener('click', () => {
-    console.log(input.value);
-  })
-}
-search()
